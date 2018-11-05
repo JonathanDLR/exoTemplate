2c81e737e3ad35c4c96a8004ab32497130ed9d97
coda2018 <codaern@gmail.com>
2018-11-05 15:37:46 +0100

    new maj

--- backend/Manager.php
+++ backend/Manager.php
@@ -9,9 +9,9 @@ abstract class Manager {
   public function __construct() {
     $this->_connexion = new Bdd();
 
-    $this->_connexion->setDns('mysql:host=192.168.3.125; dbname=jdlr300718');
-    $this->_connexion->setUser('root');
-    $this->_connexion->setMdp('sqlcoda#2018!');
+    $this->_connexion->setDns('mysql:host=; dbname=');
+    $this->_connexion->setUser('');
+    $this->_connexion->setMdp('');
 
     $this->_connexion->connect();
   }
